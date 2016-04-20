00d82481c7bed3b5f5ee12d44ad0dc6285c526dd
sadac23 <sadac23@gmail.com>
2016-04-20 23:11:05 +0900

    新規追加。

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                this._driver.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "抽選申込照会";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "抽選入力";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(37, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "取得する";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(157, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 16);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "取得しない";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(28, 59);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(266, 41);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "スクリーンショット";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 119);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "スポレク入力支援";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Reflection;
+
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Extensions;
+
+using ReadWriteCsv;
+
+namespace WindowsFormsApplication1
+{
+    /// <summary>
+    /// スポレク入力支援
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        private string _url = string.Empty;
+        private string _inputFile = string.Empty;
+        private ChromeDriver _driver = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FileVersionInfo ver = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+            this.Text = ver.Comments + "  ver." + ver.ProductVersion;
+
+            this._driver = new ChromeDriver();
+//            this._driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(Double.Parse(ConfigurationManager.AppSettings["wait_seconds"])));
+            this._driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            this._inputFile = ConfigurationManager.AppSettings["input_file"];
+        }
+
+        /// <summary>
+        /// 抽選申込結果ボタン押下処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string queryResultString = string.Empty;
+
+            try
+            {
+                // Read sample data from CSV file
+                using (CsvFileReader reader = new CsvFileReader(this._inputFile))
+                {
+                    using (CsvFileWriter writer = new CsvFileWriter(ConfigurationManager.AppSettings["output_file"]))
+                    {
+                        //セパレータの設定
+                        reader.Separator = Convert.ToChar(ConfigurationManager.AppSettings["csv_separator"].Replace(@"\t", "\t"));
+                        writer.Separator = Convert.ToChar(ConfigurationManager.AppSettings["csv_separator"].Replace(@"\t", "\t"));
+
+                        CsvRow readRow = new CsvRow();
+
+                        while (reader.ReadRow(readRow))
+                        {
+                            //抽選申込結果文字列を取得
+                            queryResultString = this.GetQueryResults(readRow[0], readRow[1]);
+
+                            //結果書き出し
+                            CsvRow writeRow = new CsvRow();
+                            writeRow.Add(readRow[0]);
+                            writeRow.Add(readRow[1]);
+                            writeRow.Add(queryResultString);
+                            writer.WriteRow(writeRow);
+
+                            if (this.GetGroupBox1Value() == this.radioButton1.Text)
+                            {
+                                SaveScreenshot(readRow[0] + ConfigurationManager.AppSettings["screenshot_file_extension"]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "システムエラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _url = "https://www.net.city.nagoya.jp/cgi-bin/sp01001";
+            OpenQA.Selenium.Chrome.ChromeDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Url = _url;
+
+            //利用者番号
+            IWebElement id = driver.FindElement(By.Name("id"));
+            id.SendKeys("1278192");
+
+            //暗証番号
+            IWebElement pass = driver.FindElement(By.Name("pass"));
+            pass.SendKeys("1234");
+
+            //地域
+            SelectElement area = new SelectElement(driver.FindElement(By.Name("area")));
+            area.SelectByValue("04");
+
+            //利用施設
+            SelectElement sisetu = new SelectElement(driver.FindElement(By.Name("sisetu")));
+            sisetu.SelectByValue("5102");
+
+            //種目
+            SelectElement syumoku = new SelectElement(driver.FindElement(By.Name("syumoku")));
+            syumoku.SelectByValue("001");
+
+            //利用月日
+            SelectElement month = new SelectElement(driver.FindElement(By.Name("month")));
+            month.SelectByValue("09");
+
+            SelectElement day = new SelectElement(driver.FindElement(By.Name("day")));
+            day.SelectByValue("06");
+
+            //供用区分
+            SelectElement time = new SelectElement(driver.FindElement(By.Name("time")));
+            time.SelectByValue("03");
+
+            //申込
+            IWebElement B1 = driver.FindElement(By.Name("B1"));
+            B1.Click();
+
+            //ダイアログ
+            OpenQA.Selenium.IAlert alert = driver.SwitchTo().Alert();
+            alert.Accept();
+
+        }
+
+        /// <summary>
+        /// 抽選申し込み情報の入力を行い申し込みボタンを押下する
+        /// </summary>
+        /// <param name="entryId">入力利用者番号</param>
+        /// <param name="entryPass">入力暗証番号</param>
+        /// <param name="entryMonth">入力月</param>
+        /// <param name="entryDay">入力日</param>
+        /// <param name="entryArea">入力地域コード</param>
+        /// <param name="entrySisetu">入力施設コード</param>
+        /// <param name="entryTime">入力時間帯コード</param>
+        private void Register(string entryId, string entryPass, string entryMonth
+                            , string entryDay, string entryArea, string entrySisetu, string entryTime)
+        {
+            try
+            {
+                //画面遷移
+                this._driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["url_sp01001"]);
+
+                //利用者番号
+                IWebElement id = this._driver.FindElement(By.Name("id"));
+                id.SendKeys(entryId);
+
+                //暗証番号
+                IWebElement pass = this._driver.FindElement(By.Name("pass"));
+                pass.SendKeys(entryPass);
+
+                //地域
+                SelectElement area = new SelectElement(this._driver.FindElement(By.Name("area")));
+                area.SelectByValue(entryArea);
+
+                //利用施設
+                SelectElement sisetu = new SelectElement(this._driver.FindElement(By.Name("sisetu")));
+                sisetu.SelectByValue(entrySisetu);
+
+                //種目
+                SelectElement syumoku = new SelectElement(this._driver.FindElement(By.Name("syumoku")));
+                syumoku.SelectByValue(ConfigurationManager.AppSettings["syumoku"]);
+
+                //利用月日
+                SelectElement month = new SelectElement(this._driver.FindElement(By.Name("month")));
+                month.SelectByValue(entryMonth);
+
+                SelectElement day = new SelectElement(this._driver.FindElement(By.Name("day")));
+                day.SelectByValue(entryDay);
+
+                //供用区分
+                SelectElement time = new SelectElement(this._driver.FindElement(By.Name("time")));
+                time.SelectByValue(entryTime);
+
+                //申込
+                IWebElement B1 = this._driver.FindElement(By.Name("B1"));
+                B1.Click();
+
+                //ダイアログ
+                B1 = this._driver.FindElement(By.Name("B1"));   //ダイアログ取得前に待機
+                OpenQA.Selenium.IAlert alert = this._driver.SwitchTo().Alert();
+                alert.Accept();
+
+                //結果取得
+                IWebElement return01001 = this._driver.FindElement(By.Name("return01001"));
+            }
+            catch(NoSuchElementException)
+            {
+                //要素取得エラーは無視
+            }
+        }
+
+        /// <summary>
+        /// 抽選申込照会結果文字列を取得する
+        /// </summary>
+        /// <param name="entryId">入力利用者番号</param>
+        /// <param name="entryPass">入力暗証番号</param>
+        /// <returns>抽選申込結果文字列</returns>
+        private string GetQueryResults(string entryId, string entryPass)
+        {
+            string resultString = string.Empty;
+
+            try
+            {
+                //画面遷移
+                this._driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["url_sp02001"]);
+
+                //利用者番号
+                IWebElement id = this._driver.FindElement(By.Name("id"));
+                id.SendKeys(entryId);
+
+                //暗証番号
+                IWebElement pass = this._driver.FindElement(By.Name("pass"));
+                pass.SendKeys(entryPass);
+
+                //ＯＫ
+                IWebElement B1 = this._driver.FindElement(By.Name("B1"));
+                B1.Click();
+
+                //結果
+                IWebElement divMain = this._driver.FindElement(By.Id("main"));
+
+                resultString = "申込あり";
+
+                //申込が存在しないかを判定
+                if (this._driver.PageSource.IndexOf(ConfigurationManager.AppSettings["not_apply_string"]) > 0)
+                {
+                    resultString = "申込なし";
+                }
+            }
+            catch(NoSuchElementException)
+            {
+                resultString = "照会失敗";
+            }
+
+            return resultString;
+        }
+
+        /// <summary>
+        /// 抽選申込ボタン押下処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string resultString = string.Empty;
+            string ssFilename = string.Empty;
+
+            try
+            {
+                // Read sample data from CSV file
+                using (CsvFileReader reader = new CsvFileReader(this._inputFile))
+                {
+                    reader.Separator = Convert.ToChar(ConfigurationManager.AppSettings["csv_separator"].Replace(@"\t","\t"));
+                    CsvRow row = new CsvRow();
+                    while (reader.ReadRow(row))
+                    {
+                        Register(row[0], row[1], row[2], row[3], row[4], row[5], row[6]);
+                        resultString = GetResultString();
+                        WriteProcessLog(row[0], row[1], resultString);
+                        if (this.GetGroupBox1Value() == this.radioButton1.Text)
+                        {
+                            ssFilename = row[0] + "_" + row[1] + "_" + row[2] + "_" + row[3] 
+                                + "_" + row[4] + "_" + row[5] + "_" + row[6] 
+                                + ConfigurationManager.AppSettings["screenshot_file_extension"];
+                            SaveScreenshot(ssFilename);
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString(),"システムエラー", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// スクリーンショットを保存する
+        /// </summary>
+        /// <param name="filename"></param>
+        private void SaveScreenshot(string filename) 
+        {
+            Screenshot ss = ((ITakesScreenshot)this._driver).GetScreenshot();
+            ss.SaveAsFile(Path.Combine(ConfigurationManager.AppSettings["screenshot_save_directry"], filename), ImageFormat.Png);
+        }
+
+        /// <summary>
+        /// 処理ログに書き込む
+        /// </summary>
+        /// <param name="entryId"></param>
+        /// <param name="entryPass"></param>
+        /// <param name="entryResultString"></param>
+        private void WriteProcessLog(string entryId, string entryPass, string entryResultString) 
+        {
+            Console.Write(entryId);
+            Console.Write(entryPass);
+            Console.Write(entryResultString);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 申し込み結果を取得する
+        /// </summary>
+        /// <returns></returns>
+        private string GetResultString()
+        {
+            return "○";
+        }
+
+        /// <summary>
+        /// GroupBox1の選択値を取得する
+        /// </summary>
+        /// <returns></returns>
+        private string GetGroupBox1Value()
+        {
+            string value = string.Empty;
+
+            //GroupBox1の選択項目を取得 
+            foreach (System.Windows.Forms.RadioButton rbSS in groupBox1.Controls)
+            {
+                if (rbSS.Checked)
+                {
+                    value = rbSS.Text;
+                }
+            }
+            return value;
+        }
+
+    }
+}
